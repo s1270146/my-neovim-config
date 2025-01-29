11ac006b0bd368a9f67741db2b15a3a58b75583f
Koyo Mori <s1270146@gmail.com>
2025-01-30 03:40:52 +0900

    fix: key mapping

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -31,11 +31,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
     set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { buffer = true })
     set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = true })
     set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { buffer = true })
-    set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", { buffer = true })
-    set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", { buffer = true })
-    set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", { buffer = true })
-    set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", { buffer = true })
-    set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { buffer = true })
+    set("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", { buffer = true })
+    set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { buffer = true })
+    set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { buffer = true })
+    set("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", { buffer = true })
+    set("n", "<space>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", { buffer = true })
   end,
 })
 
