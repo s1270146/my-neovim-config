227aa2148a4578d327aa5dbd853392a59a7cd67d
Koyo Mori <s1270146@gmail.com>
2025-02-05 21:48:31 +0900

    feat: update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -25,9 +25,9 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
     spec = {
         -- lsp
-        {"neovim/nvim-lspconfig"},
-        {"williamboman/mason.nvim"},
-        {"williamboman/mason-lspconfig.nvim"},
+        { "neovim/nvim-lspconfig" },
+        { "williamboman/mason.nvim" },
+        { "williamboman/mason-lspconfig.nvim" },
         {
             'linux-cultist/venv-selector.nvim',
             dependencies = {
@@ -57,18 +57,18 @@ require("lazy").setup({
                 table.insert(opts.sources, nls.builtins.formatting.prettierd)
             end,
         },
-       
+
 
         -- completion
-        {"L3MON4D3/LuaSnip"},
-        {"hrsh7th/nvim-cmp"},
-        {"hrsh7th/cmp-nvim-lsp"},
-        {"hrsh7th/cmp-buffer"},
-        {"saadparwaiz1/cmp_luasnip"},
-        {"cohama/lexima.vim"},
+        { "L3MON4D3/LuaSnip" },
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-buffer" },
+        { "saadparwaiz1/cmp_luasnip" },
+        { "cohama/lexima.vim" },
 
         -- icon
-        {"nvim-tree/nvim-web-devicons"},
+        { "nvim-tree/nvim-web-devicons" },
 
         -- tree
         {
@@ -94,7 +94,7 @@ require("lazy").setup({
             run = ":TSUpdate",
             config = function()
                 require("nvim-treesitter.configs").setup {
-                    ensure_installed = { "python", "lua", "javascript"},
+                    ensure_installed = { "python", "lua", "javascript" },
                     highlight = {
                         enable = true,
                     },
@@ -144,6 +144,15 @@ require("lazy").setup({
                 "MunifTanjim/nui.nvim",
                 "rcarriga/nvim-notify"
             }
+        },
+        {
+            'MeanderingProgrammer/render-markdown.nvim',
+            -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+            -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+            dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+            ---@module 'render-markdown'
+            ---@type render.md.UserConfig
+            opts = {},
         }
     },
     -- Configure any other settings here. See the documentation for more details.
@@ -152,4 +161,3 @@ require("lazy").setup({
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
-
